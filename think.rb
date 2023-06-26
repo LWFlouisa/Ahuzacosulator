a3a449eecec0fc9bac5b9e230152ae1bced22ab7
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-06-25 23:23:41 -0400

    Add files via upload

---
+++ think.rb
@@ -0,0 +1,48 @@
+require "naive_bayes"
+
+#system("ruby generate.rb; echo 'New Poem Is Generated'")
+
+sleep(1)
+
+grammar = NaiveBayes.new(:sense, :nonsense)
+
+## Sense
+grammar.train(:sense, "[  lez cabine braun ] [ es oublier hente ]", "sense") 
+grammar.train(:sense, "[   lez cabine vert ] [ es oublier hente ]", "sense")
+grammar.train(:sense, "[   lez cabine gris ] [ es oublier hente ]", "sense")
+grammar.train(:sense, "[ lez demeure braun ] [ es oublier hente ]", "sense")
+grammar.train(:sense, "[  lez demeure vert ] [ es oublier hente ]", "sense")
+grammar.train(:sense, "[  lez demeure gris ] [ es oublier hente ]", "sense")
+grammar.train(:sense, "[  lez zigner braun ] [ es oublier hente ]", "sense")
+grammar.train(:sense, "[   lez zigner vert ] [ es oublier hente ]", "sense")
+grammar.train(:sense, "[   lez zigner gris ] [ es oublier hente ]", "sense")
+
+## Nonsense
+grammar.train(:nonsense, "[ es oublier hente ] [  lez cabine braun ]", "nonsense") 
+grammar.train(:nonsense, "[ es oublier hente ] [   lez cabine vert ]", "nonsense")
+grammar.train(:nonsense, "[ es oublier hente ] [   lez cabine gris ]", "nonsense")
+grammar.train(:nonsense, "[ es oublier hente ] [ lez demeure braun ]", "nonsense")
+grammar.train(:nonsense, "[ es oublier hente ] [  lez demeure vert ]", "nonsense")
+grammar.train(:nonsense, "[ es oublier hente ] [  lez demeure gris ]", "nonsense")
+grammar.train(:nonsense, "[ es oublier hente ] [  lez zigner braun ]", "nonsense")
+grammar.train(:nonsense, "[ es oublier hente ] [   lez zigner vert ]", "nonsense")
+grammar.train(:nonsense, "[ es oublier hente ] [   lez zigner gris ]", "nonsense")
+
+new_phrase = File.readlines("_imaginedpath/outcomes/input.txt")
+
+row = 0
+
+iteration = new_phrase.size.to_i
+
+iteration.times do
+  result = grammar.classify(*new_phrase[row])
+
+  first_label        = result[0]
+  first_probability  = result[1]
+  second_label       = "non#{first_label}"
+  second_probability = first_probability / 100
+
+  puts "#{first_label}: #{first_probability} #{second_label}: #{second_probability}"
+
+  row = row + 1
+end
